9471cf68a397140f418994fe2857b561efff9603
Dylan Albone <da338@campus.cs.le.ac.uk>
2026-03-07 12:45:48 +0000

    dependencies and that

---
+++ HerTrack/src/main/java/com/hackathon/hertrack/controller/ChatController.java
@@ -0,0 +1,16 @@
+package com.hackathon.hertrack.controller;
+
+import com.hackathon.hertrack.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/messages")
+    public ChatMessage sendMessage(ChatMessage message) {
+        return message;
+    }
+}
