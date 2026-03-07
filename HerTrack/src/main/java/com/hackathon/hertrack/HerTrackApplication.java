1ae512558cd692c2533939e25daae7e4a771f0fe
Gabriel <gabrielsaunders410@gmail.com>
2026-03-07 11:56:32 +0000

    Created the project.

---
+++ HerTrack/src/main/java/com/hackathon/hertrack/HerTrackApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.hertrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HerTrackApplication.class, args);
+    }
+
+}
---
+++ HerTrack/src/test/java/com/hackathon/hertrack/HerTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.hertrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
