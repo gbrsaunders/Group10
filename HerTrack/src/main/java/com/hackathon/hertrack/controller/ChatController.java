11b38baf64139c6baf62c457c78146049c239df0
Dylan Albone <zerotntgamer101@gmail.com>
2026-03-09 14:07:22 +0000

    chatroom

--- HerTrack/src/main/java/com/hackathon/hertrack/controller/ChatController.java
+++ HerTrack/src/main/java/com/hackathon/hertrack/controller/ChatController.java
@@ -1,22 +1,84 @@
 package com.hackathon.hertrack.controller;
 
 import com.hackathon.hertrack.model.ChatMessage;
+import com.hackathon.hertrack.repo.ChatRepository;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class ChatController {
 
-    @MessageMapping("/sendMessage")
-    @SendTo("/topic/messages")
-    public ChatMessage sendMessage(ChatMessage message) {
-        return message;
+    private final ChatRepository chatRepository;
+    public ChatController(ChatRepository chatRepository) {
+        this.chatRepository = chatRepository;
     }
 
-    @GetMapping("/chat")
-    public String chat() {
-        return "chat";
+    @MessageMapping("/sendMessage/general")
+    @SendTo("/topic/messages/general")
+    public ChatMessage sendMessageGeneral(@Payload ChatMessage message) {
+        message.setRoom("general");
+        return chatRepository.save(message);
+    }
+
+    @MessageMapping("/sendMessage/beauty")
+    @SendTo("/topic/messages/beauty")
+    public ChatMessage sendMessageBeauty(@Payload ChatMessage message) {
+        message.setRoom("beauty");
+        return chatRepository.save(message);
+    }
+
+    @MessageMapping("/sendMessage/relationships")
+    @SendTo("/topic/messages/relationships")
+    public ChatMessage sendMessageRelationships(@Payload ChatMessage message) {
+        message.setRoom("relationships");
+        return chatRepository.save(message);
+    }
+
+    @MessageMapping("/sendMessage/endometriosis")
+    @SendTo("/topic/messages/endometriosis")
+    public ChatMessage sendMessageEndo(@Payload ChatMessage message) {
+        message.setRoom("endometriosis");
+        return chatRepository.save(message);
+    }
+
+    @GetMapping("/chat/{roomName}")
+    public String chat(@PathVariable String roomName,
+                       Model model) {
+        switch (roomName) {
+            case "beauty" -> {
+                List<ChatMessage> allBeautyWellChats = chatRepository.findTop10ByRoomOrderByLocaldatetimeAsc("beauty");
+                model.addAttribute("allChats", allBeautyWellChats);
+                return "beautyChat";
+            }
+            case "relationships" -> {
+                List<ChatMessage> allRelationshipChats = chatRepository.findTop10ByRoomOrderByLocaldatetimeAsc("relationships");
+                model.addAttribute("allChats", allRelationshipChats);
+                return "relationshipsChat";
+            }
+            case "endometriosis" -> {
+                List<ChatMessage> allEndoChats = chatRepository.findTop10ByRoomOrderByLocaldatetimeAsc("endometriosis");
+                model.addAttribute("allChats", allEndoChats);
+                return "endoChat";
+            }
+            default -> {
+                List<ChatMessage> allGeneralChats = chatRepository.findTop10ByRoomOrderByLocaldatetimeAsc("general");
+                model.addAttribute("allChats", allGeneralChats);
+                return "GeneralChat";
+            }
+        }
+
+    }
+
+    @RequestMapping("/forum")
+    public String forum() {
+        return "forum";
     }
 }
